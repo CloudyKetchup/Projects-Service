d7e9ac842b705bb480653737c662d31a696ee5dc
krypton17 <maxdodon25@gmail.com>
2019-05-18 10:51:15 -0700

    code cleanup

--- src/main/kotlin/com/krypton/projectsrepo/model/Project.kt
+++ src/main/kotlin/com/krypton/projectsrepo/model/Project.kt
@@ -6,18 +6,18 @@ import javax.persistence.*
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Project(
-		@Id		var id 			: Long?   = null,
-		@Column var name 		: String? = null,
-		@Column var description : String? = null,
-		@Column var language 	: String? = null,
+		@Id		var id			: Long?   = null,
+		@Column var name		: String? = null,
+		@Column var description	: String? = null,
+		@Column var language	: String? = null,
 		@Column var url			: String? = null
 ) {
 	override fun toString() : String {
 		return 	"Project { \n" +
-				"id				-> $id 			\n" +
-				"title			-> $name 		\n" +
-				"description 	-> $description \n" +
-				"language 	 	-> $language 	\n" +
-				"url 		 	-> $url 		\n }"
+				"id				-> $id			\n" +
+				"title			-> $name		\n" +
+				"description	-> $description	\n" +
+				"language		-> $language	\n" +
+				"url			-> $url			\n }"
 	}
 }
